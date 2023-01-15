4a2e150a9686d6c22d1865eb342da2efb24626bf
Joe Binns <joebinns.95@gmail.com>
2023-01-15 16:56:21 +0100

    Add non-functioning UI window

---
+++ Assets/Tool/UI Toolkit/Custom/GameSettings/GameSettings.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GameSettings : EditorWindow
+{
+    [SerializeField]
+    private VisualTreeAsset m_VisualTreeAsset = default;
+
+    [MenuItem("Window/UI Toolkit/GameSettings")]
+    public static void ShowExample()
+    {
+        GameSettings wnd = GetWindow<GameSettings>();
+        wnd.titleContent = new GUIContent("GameSettings");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+        
+        // Instantiate UXML
+        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(labelFromUXML);
+    }
+}
