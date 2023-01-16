ace99539aa87a28cda3e009ac377d29511627291
Joe Binns <joebinns.95@gmail.com>
2023-01-16 10:49:57 +0100

    Add (miss-guided) Unity documentation bindings example

--- Assets/Tool/UI Toolkit/Custom/GameSettings/GameSettings.cs
+++ Assets/Tool/UI Toolkit/Custom/GameSettings/GameSettings.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -22,5 +23,36 @@ public class GameSettings : EditorWindow
         // Instantiate UXML
         VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
         root.Add(labelFromUXML);
+        
+        //OnSelectionChange(); // TODO: I don't think this is the binding I wanted... Why is it talking about game objects?
+    }
+    
+    /*
+    public void OnSelectionChange()
+    {
+        GameObject selectedObject = Selection.activeObject as GameObject;
+        if (selectedObject != null)
+        {
+            // Create serialization object
+            SerializedObject so = new SerializedObject(selectedObject);
+            // Bind it to the root of the hierarchy. It will find the right object to bind to...
+            rootVisualElement.Bind(so);
+        }
+        else
+        {
+            // Unbind the object from the actual visual element
+            rootVisualElement.Unbind();
+              
+            // Clear the TextField after the binding is removed
+            // (this code is not safe if the Q() returns null)
+            rootVisualElement.Q<Foldout>("engine").Q<VisualElement>("unity-content").Q<Slider>("throttle-power").value = 0;
+            //rootVisualElement.Q<Slider>("Throttle Power").value = 0;
+        }
+    }
+    */
+
+    private void OnValidate()
+    {
+        //Debug.Log(rootVisualElement.Q<Slider>("Throttle Power").value);
     }
 }
