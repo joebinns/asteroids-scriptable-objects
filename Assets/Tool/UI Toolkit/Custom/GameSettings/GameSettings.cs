4047b0016a70666d723f7512983a66a6169cdcc7
Joe Binns <joebinns.95@gmail.com>
2023-01-16 12:14:43 +0100

    Remove rendundant templates

--- Assets/Tool/UI Toolkit/Custom/GameSettings/GameSettings.cs
+++ Assets/Tool/UI Toolkit/Custom/GameSettings/GameSettings.cs
@@ -1,5 +1,4 @@
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +7,8 @@ public class GameSettings : EditorWindow
     [SerializeField]
     private VisualTreeAsset m_VisualTreeAsset = default;
 
+    public float m_InitialHealth;
+    
     [MenuItem("Window/UI Toolkit/GameSettings")]
     public static void ShowExample()
     {
@@ -23,36 +24,10 @@ public class GameSettings : EditorWindow
         // Instantiate UXML
         VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
         root.Add(labelFromUXML);
-        
-        //OnSelectionChange(); // TODO: I don't think this is the binding I wanted... Why is it talking about game objects?
     }
     
-    /*
-    public void OnSelectionChange()
+    private void OnInspectorUpdate()
     {
-        GameObject selectedObject = Selection.activeObject as GameObject;
-        if (selectedObject != null)
-        {
-            // Create serialization object
-            SerializedObject so = new SerializedObject(selectedObject);
-            // Bind it to the root of the hierarchy. It will find the right object to bind to...
-            rootVisualElement.Bind(so);
-        }
-        else
-        {
-            // Unbind the object from the actual visual element
-            rootVisualElement.Unbind();
-              
-            // Clear the TextField after the binding is removed
-            // (this code is not safe if the Q() returns null)
-            rootVisualElement.Q<Foldout>("engine").Q<VisualElement>("unity-content").Q<Slider>("throttle-power").value = 0;
-            //rootVisualElement.Q<Slider>("Throttle Power").value = 0;
-        }
-    }
-    */
-
-    private void OnValidate()
-    {
-        //Debug.Log(rootVisualElement.Q<Slider>("Throttle Power").value);
+        Debug.Log(m_InitialHealth);
     }
 }
