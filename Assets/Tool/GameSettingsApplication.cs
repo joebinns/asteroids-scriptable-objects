003102cd6d36a8dbcb45f47880ed475b2f1c5008
Joe Binns <joebinns.95@gmail.com>
2023-01-17 11:56:39 +0100

    Add content-awareness to the application of game settings

--- Assets/Tool/GameSettingsApplication.cs
+++ Assets/Tool/GameSettingsApplication.cs
@@ -1,14 +1,15 @@
+using System.Collections.Generic;
 using Asteroids;
 using Ship;
 using UnityEngine;
 
+// TODO: Assign a component type to each settings struct. (in struct).
+// TODO: For each struct, get all instances of it's component type in the scene.
+// TODO: For each instance of component type, set the components variables to be that of the struct. (as is already done)
+
 public class GameSettingsApplication : MonoBehaviour
 {
     [SerializeField] private GameSettings _gameSettings;
-    [SerializeField] private AsteroidSpawner _asteroidSpawner;
-    [SerializeField] private Engine _engine;
-    [SerializeField] private Hull _hull;
-    [SerializeField] private Gun _gun;
 
     private void Start()
     {
@@ -25,26 +26,43 @@ public class GameSettingsApplication : MonoBehaviour
 
     private void ApplyAsteroidSpawnerSettings()
     {
-        _asteroidSpawner.MinSpawnTime = _gameSettings.AsteroidSpawnerSettings.SpawnTimeRange.x;
-        _asteroidSpawner.MaxSpawnTime = _gameSettings.AsteroidSpawnerSettings.SpawnTimeRange.y;
-        _asteroidSpawner.MinAmount = _gameSettings.AsteroidSpawnerSettings.AmountRange.x;
-        _asteroidSpawner.MaxAmount = _gameSettings.AsteroidSpawnerSettings.AmountRange.y;
+        foreach (var asteroidSpawner in GetAsteroidSpawners())
+        {
+            asteroidSpawner.MinSpawnTime = _gameSettings.AsteroidSpawnerSettings.SpawnTimeRange.x;
+            asteroidSpawner.MaxSpawnTime = _gameSettings.AsteroidSpawnerSettings.SpawnTimeRange.y;
+            asteroidSpawner.MinAmount = _gameSettings.AsteroidSpawnerSettings.AmountRange.x;
+            asteroidSpawner.MaxAmount = _gameSettings.AsteroidSpawnerSettings.AmountRange.y;
+        }
     }
     
     private void ApplyEngineSettings()
     {
-        _engine.ThrottlePower = _gameSettings.EngineSettings.ThrottlePower;
-        _engine.RotationPower = _gameSettings.EngineSettings.RotationPower;
+        foreach (var engine in GetEngines())
+        {
+            engine.ThrottlePower = _gameSettings.EngineSettings.ThrottlePower;
+            engine.RotationPower = _gameSettings.EngineSettings.RotationPower;
+        }
     }
 
     private void ApplyHullSettings()
     {
-        _hull.Health = _gameSettings.HullSettings.InitialHealth;
+        foreach (var hull in GetHulls())
+        {
+            hull.Health = _gameSettings.HullSettings.InitialHealth;
+        }
     }
 
     private void ApplyGunSettings()
     {
-        _gun.Cooldown = _gameSettings.GunSettings.Cooldown;
+        foreach (var gun in GetGuns())
+        {
+            gun.Cooldown = _gameSettings.GunSettings.Cooldown;
+        }
     }
+    
+    private IEnumerable<AsteroidSpawner> GetAsteroidSpawners() => FindObjectsOfType<AsteroidSpawner>();
+    private IEnumerable<Engine> GetEngines() => FindObjectsOfType<Engine>();
+    private IEnumerable<Hull> GetHulls() => FindObjectsOfType<Hull>();
+    private IEnumerable<Gun> GetGuns() => FindObjectsOfType<Gun>();
 }
 
